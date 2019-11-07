7fb31688f7dcfd4414cd631a75a9de07a6874cd7
Evan Sia <evan.sia@alphasights.com>
2019-11-07 09:26:30 +0000

    init

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(5100);
+        app.get("/", ctx -> ctx.result("Hello World!"));
+    }
+}
